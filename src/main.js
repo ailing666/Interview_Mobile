b7b7637361b60d4b8b1d3744c2645e221cc503a9
xatongxue <1120877019@qq.com>
2020-07-24 21:14:14 +0800

    引入Vant

--- src/main.js
+++ src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入Vant
+import Vant from 'vant'
+// 引入样式
+import 'vant/lib/index.css'
+// 注册Vant
+Vue.use(Vant)
 
 Vue.config.productionTip = false
 
