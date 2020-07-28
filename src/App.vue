6472ebe39b137c73b9861b0719c46a008bd34aa9
xatongxue <1120877019@qq.com>
2020-07-28 15:15:37 +0800

    tabbar完成

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,46 @@
 <template>
-  <div class="app">app</div>
+  <div class="app">
+    <!-- 路由出口 -->
+    <router-view></router-view>
+    <!-- route:开启路由模式
+      active-color: 选中颜色
+      inactive-color:初始颜色
+      $route.meta.showTabBar:当路由元中标记showTabBar为true才让tabbar显示
+    -->
+    <van-tabbar
+      route
+      active-color="#e40137"
+      inactive-color="#b4b4bd"
+      v-show="$route.meta.showTabBar"
+    >
+      <van-tabbar-item
+        v-for="(item, index) in routerOptions"
+        :key="index"
+        :to="item.path"
+      >
+        <span>{{ item.meta.title }} </span>
+        <template #icon>
+          <i class="iconfont" :class="item.meta.icon"></i>
+        </template>
+      </van-tabbar-item>
+    </van-tabbar>
+  </div>
 </template>
 
 <script>
-export default {}
+export default {
+  name: 'app',
+  computed: {
+    // 利用计算属性,截取路由中四个需显示的tabbar
+    routerOptions () {
+      return this.$router.options.routes.slice(1, 5)
+    }
+  }
+}
 </script>
 
-<style></style>
+<style lang="less">
+.iconfont {
+  font-size: 28px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,63 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 
-Vue.use(VueRouter)
+import Company from '@/views/company/Company.vue'
+import Question from '@/views/question/Question.vue'
+import Find from '@/views/find/Find.vue'
+import My from '@/views/my/My.vue'
+import Login from '@/views/login/Login.vue'
 
+Vue.use(VueRouter)
 const router = new VueRouter({
-  routes: []
+  routes: [
+    {
+      path: '/',
+      redirect: '/my'
+    },
+    {
+      path: '/company',
+      component: Company,
+      meta: {
+        title: '公司',
+        icon: 'iconicon_footbar_gs_sel',
+        showTabBar: true
+      }
+    },
+    {
+      path: '/question',
+      component: Question,
+      meta: {
+        title: '刷题',
+        icon: 'iconicon_footbar_st_sel',
+        showTabBar: true
+      }
+    },
+    {
+      path: '/find',
+      component: Find,
+      meta: {
+        title: '发现',
+        icon: 'iconicon_footbar_fx_nor',
+        showTabBar: true
+      }
+    },
+    {
+      path: '/my',
+      component: My,
+      meta: {
+        title: '我的',
+        icon: 'iconicon_footbar_wode_nor',
+        showTabBar: true
+      }
+    },
+    {
+      path: '/login',
+      component: Login,
+      meta: {
+        title: '登录'
+      }
+    }
+  ]
 })
 
 export default router
