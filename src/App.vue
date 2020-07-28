076171c6ea14397511b7cb68295dcbe8b101b36e
xatongxue <1120877019@qq.com>
2020-07-28 14:39:33 +0800

    重置

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,9 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
-    <!-- 实现tabbar -->
-    <van-tabbar
-      route
-      active-color="#e40137"
-      inactive-color="#b4b4bd"
-      v-show="$route.meta.isShowTabbar"
-    >
-      <van-tabbar-item
-        v-for="(item, index) in routerOptions"
-        :key="index"
-        :to="item.path"
-      >
-        <span>{{ item.mate.title }}</span>
-        <template #icon>
-          <i class="iconfont" :class="item.mate.icon"></i>
-        </template>
-      </van-tabbar-item>
-    </van-tabbar>
-  </div>
+  <div class="app">app</div>
 </template>
 
 <script>
-export default {
-  name: 'App',
-  computed: {
-    routerOptions () {
-      return this.$router.options.routes.slice(1, 5)
-    }
-  }
-}
+export default {}
 </script>
 
 <style></style>
